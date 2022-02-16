c8adad5d0d88e365d7eefa57b4571ea795d9ba7a
tdwork-cz <91006489+tdwork-cz@users.noreply.github.com>
2022-02-16 09:56:01 +0100

    - add environmentObject in RecipeTabView.swift and ContentView.swift - replace List with ScrollView,LazyStack and ForEach  in ContentView.swift - hide navigationBarTitle and use Text element instead

--- RecipeApp/View/ContentView.swift
+++ RecipeApp/View/ContentView.swift
@@ -13,27 +13,39 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView {
-            List(model.recipies) {r in
-                NavigationLink {
-                    RecipeDetailView(recipe: r)
-                } label: {
-                    HStack(spacing: 25) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 70, height: 70, alignment: .center)
-                            .clipped()
-                            .cornerRadius(10)
-                        Text(r.name)
+            VStack(alignment: .leading) {
+                Text("All Recipies")
+                    .bold()
+                    .padding(.top, 30)
+                    .font(.largeTitle)
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipies) {r in
+                            NavigationLink {
+                                RecipeDetailView(recipe: r)
+                            } label: {
+                                HStack(spacing: 25) {
+                                    Image(r.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 70, height: 70, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(10)
+                                    Text(r.name)
+                                        .foregroundColor(.black)
+                                }
+                            }
+                        }
                     }
                 }
-            }.navigationTitle("All Recepies")
-        }
+            }.navigationBarHidden(true)
+        }.padding(.leading)
     }
 }
 
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
