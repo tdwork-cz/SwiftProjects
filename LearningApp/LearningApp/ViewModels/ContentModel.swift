c0be7f3d08992c0192f4b237e7111462fe48f8cc
tdwork-cz <91006489+tdwork-cz@users.noreply.github.com>
2022-02-22 19:57:04 +0100

    - add assets - add data files - parse json in content model - parse style data in content model

--- LearningApp/LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/LearningApp/ViewModels/ContentModel.swift
@@ -8,5 +8,38 @@
 import Foundation
 
 class ContentModel:ObservableObject {
+    @Published var modules:[Module] = [Module]()
+    var styleData:Data?
     
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        let pathJSON = Bundle.main.path(forResource: "data", ofType: "json")
+        
+        if let path = pathJSON {
+            let url = URL(fileURLWithPath: path)
+            
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let modulesData = try decoder.decode([Module].self, from: data)
+                
+                self.modules = modulesData
+            }
+            catch {
+                print(error.self)
+            }
+        }
+        
+        let urlHTML = Bundle.main.url(forResource: "style", withExtension: "html")
+        do {
+            let data = try Data(contentsOf: urlHTML!)
+            self.styleData = data
+        }
+        catch {
+            print(error.self)
+        }
+    }
 }
