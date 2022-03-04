83f034e50e1302edb6c18dbda060437294e92ae9
tdwork-cz <91006489+tdwork-cz@users.noreply.github.com>
2022-03-04 19:48:00 +0100

    - set up folders for views, models and view models - rename default ContentView to LaunchView

---
+++ CitySights/CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Тимофей Дудич on 4.03.22.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LaunchView()
+        }
+    }
+}
---
+++ CitySights/CitySights/Views/LaunchView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CitySights
+//
+//  Created by Тимофей Дудич on 4.03.22.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
